a000a3335df93cea843962474feea0aea346bcb1
Will Turbaevsky <turbaevsky@gmail.com>
2020-04-30 21:57:02 +0100

    replace pivot with pandas

--- who.py
+++ who.py
@@ -1,36 +1,24 @@
 #!/usr/bin/python3
 
 from datetime import datetime
-from tqdm import tqdm
 import logging
 import sys
 
-logging.basicConfig(filename='app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s',
+datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
-def pivot(df,cols=4):
-    
-    import pandas as pd
 
-    l = len(df.iloc[0])-4
-    col = df.columns
-    d = pd.DataFrame()
-    for i in tqdm(range(len(df))):
-        #print('.',end='')
-        #print(df.iloc[i])
-        for j in range(l):
-            #print(df.iloc[i][j], type(df.iloc[i][j]))
-            dd = pd.DataFrame(data={'Province/State':[df.iloc[i][0]],'Country/Region':[df.iloc[i][1]],\
-                      'Lat':[df.iloc[i][2]],'Long':[df.iloc[i][3]],'Date':[col[j+cols]],\
-                      'value':[df.iloc[i][j+cols]]})
-            #print(dd)
-            d = d.append(dd, ignore_index=True)
-            #print(d)
-    d['Date'] = d.Date.apply(lambda x:(datetime.strptime(x,'%m/%d/%y').strftime("%Y-%m-%d")))
-    return d
+def pivot(df):
+    df = df.fillna('main')
+    df = df.pivot_table(columns=['Province/State','Country/Region','Lat','Long'])
+    df = df.reset_index()
+    df['Date'] = df.level_0.apply(lambda x:(datetime.strptime(x,'%m/%d/%y').strftime("%Y-%m-%d")))
+    df.drop(columns=['level_0'], inplace=True)
+    df.columns = ['Province/State','Country/Region','Lat','Long','value','Date']
+    return df
 
 
 def main():
-	# TODO Add check whether file been modified checking md5
 	import pandas as pd
 
 	confirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
@@ -69,9 +57,9 @@ def main():
 	#print('*',end='')
 	recovered = pivot(recovered)
 
-	confirmed.columns = ['Province/State', 'Country/Region', 'Lat', 'Long', 'Date', 'Confirmed']
-	dead.columns = ['Province/State', 'Country/Region', 'Lat', 'Long', 'Date', 'Dead']
-	recovered.columns = ['Province/State', 'Country/Region', 'Lat', 'Long', 'Date', 'Recovered']
+	confirmed.columns = ['Province/State', 'Country/Region', 'Lat', 'Long', 'Confirmed', 'Date']
+	dead.columns = ['Province/State', 'Country/Region', 'Lat', 'Long', 'Dead', 'Date']
+	recovered.columns = ['Province/State', 'Country/Region', 'Lat', 'Long', 'Recovered', 'Date']
 
 	data = confirmed.merge(recovered, on=['Date','Province/State','Country/Region'], how='left').drop(columns=['Lat_y','Long_y','Lat_x','Long_x'])
 	data = data.merge(dead, on=['Date','Province/State','Country/Region'], how='left')#.drop(columns=['Lat','Long'])
